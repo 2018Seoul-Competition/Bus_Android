9f82047c1d672e795dc2482358c6f68917eeacb9
MangKyu <whalsrb1226@naver.com>
2018-09-06 14:52:13 +0900

    Update Station Activity with Dagger2

--- app/src/main/java/com/ndc/bus/Activity/StationActivity.java
+++ app/src/main/java/com/ndc/bus/Activity/StationActivity.java
@@ -2,36 +2,43 @@ package com.ndc.bus.Activity;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
 import com.ndc.bus.Activity.Adapter.StationAdapter;
+import com.ndc.bus.Database.BusDatabase;
+import com.ndc.bus.Database.BusDatabaseClient;
+import com.ndc.bus.Database.BusDatabaseModule;
 import com.ndc.bus.Listener.StationRecyclerViewClickListener;
 import com.ndc.bus.R;
 import com.ndc.bus.Service.ArrivalNotificationService;
-import com.ndc.bus.Station.Station;
-import com.ndc.bus.Utils.Dlog;
-import com.ndc.bus.databinding.ActivityStationBinding;
+    import com.ndc.bus.Station.Station;
 
-import java.util.ArrayList;
+    import com.ndc.bus.Utils.Dlog;
+    import com.ndc.bus.databinding.ActivityStationBinding;
 
-public class StationActivity extends BaseActivity {
+    import java.util.ArrayList;
 
+    import javax.inject.Inject;
+
+        public class StationActivity extends BaseActivity {
+            @Inject
+            BusDatabaseClient busDatabaseClient;
     private ActivityStationBinding binding;
     private ArrayList<Station> stationList;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BusDatabase busDatabase = busDatabaseClient.getBusDatabase();
+        stationList = busDatabase.stationDAO().getAllStations();
+    }
 
     @Override
     public void initSettings(){
         super.initSettings();
         binding = DataBindingUtil.setContentView(this, R.layout.activity_station);
         binding.setActivity(this);
-        stationList = new ArrayList<>();
-
-        Station station = new Station("버스 번호", "정류장 이름");
-        stationList.add(station);
-        stationList.add(station);
-        stationList.add(station);
-        stationList.add(station);
 
         StationAdapter stationAdapter = new StationAdapter(this, stationList, new StationRecyclerViewClickListener() {
             @Override
