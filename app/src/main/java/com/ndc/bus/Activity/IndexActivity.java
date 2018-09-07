2bc6932a8b38d69c3718f7eb230f970560095ce5
MangKyu <whalsrb1226@naver.com>
2018-09-07 17:52:47 +0900

    Create AsyncTask to insert Data

--- app/src/main/java/com/ndc/bus/Activity/IndexActivity.java
+++ app/src/main/java/com/ndc/bus/Activity/IndexActivity.java
@@ -2,6 +2,7 @@ package com.ndc.bus.Activity;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
@@ -11,9 +12,16 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.ndc.bus.Database.BusDatabaseClient;
+import com.ndc.bus.Database.BusDatabaseModule;
 import com.ndc.bus.R;
 
+import javax.inject.Inject;
+
 public class IndexActivity extends BaseActivity{
+
+    @Inject
+    BusDatabaseClient busDatabaseClient;
 
     private static final int TIME_GIF = 1000 * 3;
     private Handler mHandler;
@@ -39,6 +47,8 @@ public class IndexActivity extends BaseActivity{
         }
 
         //DB 호출
+        InitDatabaseTask initTask = new InitDatabaseTask();
+        initTask.execute();
 
         //처음 화면 보여주기
         showStartAni();
@@ -56,4 +66,14 @@ public class IndexActivity extends BaseActivity{
         GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(iv);
         Glide.with(this).load(R.raw.chu).into(iv);
     }
+
+    private class InitDatabaseTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(final Void... params) {
+            busDatabaseClient.initBusData();
+            // 데이터베이스에 데이터를 넣은 후이므로 여기서 화면 이동을 해야함
+            return null;
+        }
+    }
+
 }
