8239e8cf930990f5a0defbf6e2c6561d0ed7a9fd
MangKyu <whalsrb1226@naver.com>
2018-09-07 17:20:16 +0900

    Dagger2 with Room Database

--- app/src/main/java/com/ndc/bus/Database/BusDatabase.java
+++ app/src/main/java/com/ndc/bus/Database/BusDatabase.java
@@ -10,9 +10,7 @@ import com.ndc.bus.Route.RouteRowDAO;
 import com.ndc.bus.Station.Station;
 import com.ndc.bus.Station.StationDAO;
 
-@Database(version = 1, entities = {Route.class, RouteRow.class, Station.class})
+@Database(version = 1, entities = {Station.class, Route.class, RouteRow.class})
 public abstract class BusDatabase extends RoomDatabase {
-    abstract public RouteDAO routeDAO();
-    abstract public RouteRowDAO routeRowDAO();
     abstract public StationDAO stationDAO();
 }
--- app/src/main/java/com/ndc/bus/Database/BusDatabaseClient.java
+++ app/src/main/java/com/ndc/bus/Database/BusDatabaseClient.java
@@ -14,42 +14,39 @@ import javax.inject.Singleton;
 
 @Singleton
 public class BusDatabaseClient {
-    private static final String DB_NAME = "BUS_SEOUL_COMPETITION";
+    private static final String DB_NAME = "BUS-SEOUL-COMPETITION";
+    //@Inject
     Context context;
     private BusDatabase busDatabase;
 
-    public BusDatabaseClient(Context context){
+    BusDatabaseClient(Context context){
+        this.busDatabase = Room.databaseBuilder(context.getApplicationContext(), BusDatabase.class, DB_NAME).build();
         this.context = context;
     }
 
-    private void initBusDatabase(){
-        boolean isExist = isDatabaseExists();
-        BusDatabase busDatabase = Room.databaseBuilder(context, BusDatabase.class, DB_NAME).build();
-        if(!isExist){
-            initData();
-        }
-        this.busDatabase = busDatabase;
-    }
-
     public BusDatabase getBusDatabase() {
-        initBusDatabase();
         return busDatabase;
     }
 
     private boolean isDatabaseExists(){
-        File dbFile = context.getDatabasePath(DB_NAME);
-        return dbFile.exists();
+        int stationSize = busDatabase.stationDAO().getAllStations().size();
+        if(stationSize <= 0 ){
+            return false;
+        }
+        return true;
     }
 
-    private void initData(){
-        ArrayList<Station> stationList = new ArrayList<>();
-        Station station1 = new Station("A", "에이", "1", "일");
-        stationList.add(station1);
-        Station station2 = new Station("B", "비", "2", "이");
-        stationList.add(station2);
-        Station station3 = new Station("C", "씨", "3", "삼");
-        stationList.add(station3);
+    public void initBusData(){
+        boolean isExist = isDatabaseExists();
+        if(!isExist){
+            ArrayList<Station> stationList = new ArrayList<>();
+            Station station1 = new Station("A", "A", "A", "A");
+            stationList.add(station1);
 
-        busDatabase.stationDAO().insertAllStations(stationList);
+            Station station2 = new Station("B", "B", "B", "B");
+            stationList.add(station2);
+            busDatabase.stationDAO().insertAllStations(stationList);
+
+        }
     }
 }
