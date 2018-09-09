fd6725a1d2b0aff88d06669e72e16f6dbef0c157
201411317 <whalsrb1226@naver.com>
2018-09-10 01:39:31 +0900

    Remove Comment

--- app/src/main/java/com/ndc/bus/Database/BusDatabaseModule.java
+++ app/src/main/java/com/ndc/bus/Database/BusDatabaseModule.java
@@ -7,10 +7,6 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 
-//https://github.com/poqw/toys/blob/master/android/Dagger2toy/app/src/main/java/com/dev/poqw/dagger2toy/di/ActivityModule.java
-//https://poqw.github.io/di_2/
-
-
 @Module
 public class BusDatabaseModule {
     @Singleton
